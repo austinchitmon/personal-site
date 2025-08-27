868b0512825da5134a74b543d8bc0b33103bbb31
austinchitmon <austinchitmon@gmail.com>
2025-08-27 10:03:12 -0500

    generate ts manifest file for build time execution

--- src/app/features/chittylog/chittylog-container/chittylog-container.component.ts
+++ src/app/features/chittylog/chittylog-container/chittylog-container.component.ts
@@ -1,25 +1,15 @@
 import { NgOptimizedImage } from '@angular/common';
-import {
-  Component,
-  inject,
-  Signal
-} from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { Component, signal, Signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Card } from 'primeng/card';
-import {
-  map,
-  tap
-} from 'rxjs';
-import { ApiService } from '../../../shared/api/api.service';
-
+import { BLOG_MANIFEST } from '../../../shared/data/blog-manifest';
 
 interface BlogConfig {
   fileName: Markdown;
   title: string;
   subtitle: string;
   date: string;
-  tags: string[];
+  tags: readonly string[];
 }
 
 type Markdown = `${string}.md`;
@@ -34,57 +24,49 @@ interface Article extends BlogConfig {
 
 @Component({
   selector: 'app-chittylog-container',
-  imports: [
-    Card,
-    NgOptimizedImage,
-    RouterLink
-  ],
+  imports: [Card, NgOptimizedImage, RouterLink],
   template: `
     <div class="page-container">
-      <h1><span class="title white">Chitty</span><span class="title gradient">blog</span></h1>
-      <h3 class="subtitle">Guides, updates, and random thoughts directly from me</h3>
+      <h1>
+        <span class="title white">Chitty</span
+        ><span class="title gradient">blog</span>
+      </h1>
+      <h3 class="subtitle">
+        Guides, updates, and random thoughts directly from me
+      </h3>
       <div class="card-container">
         @for (article of articles(); track $index) {
-          <p-card class="card"
-                  [routerLink]="article.routerLink">
-            <ng-template #header>
-              <div class="box">
-                <img alt="Card"
-                     class="img-fill"
-                     [ngSrc]="'blog/' + article.routerLink + '.png'"
-                     fill/>
-              </div>
-            </ng-template>
-            <ng-template #title>{{article.title || 'Blank'}}</ng-template>
-            <ng-template #subtitle>{{article.date}}</ng-template>
-            <p class="multiline-ellipsis">{{article.subtitle}}</p>
-          </p-card>
+        <p-card class="card" [routerLink]="article.routerLink">
+          <ng-template #header>
+            <div class="box">
+              <img
+                alt="Card"
+                class="img-fill"
+                [ngSrc]="'blog/' + article.routerLink + '.png'"
+                fill
+              />
+            </div>
+          </ng-template>
+          <ng-template #title>{{ article.title || 'Blank' }}</ng-template>
+          <ng-template #subtitle>{{ article.date }}</ng-template>
+          <p class="multiline-ellipsis">{{ article.subtitle }}</p>
+        </p-card>
         }
       </div>
     </div>
   `,
-  styleUrl: './chittylog-container.component.scss'
+  styleUrl: './chittylog-container.component.scss',
 })
 export class ChittylogContainerComponent {
-  api = inject(ApiService);
-  articles: Signal<Article[]> = toSignal(
-    this.api.get<Manifest>('blog/manifest.json').pipe(
-      map((manifest: Manifest): Article[] => {
-        return [
-          ...manifest.files.map(fileConfig => this.formatFile(fileConfig)),
-        ];
-      }),
-      tap(files => console.log(files))
-    ),
-    { initialValue: [] }
+  articles: Signal<Article[]> = signal(
+    BLOG_MANIFEST.files.map((fileConfig) => this.formatFile(fileConfig))
   );
 
   private formatFile(file: BlogConfig): Article {
     return {
       ...file,
       date: new Date(file.date).toDateString(),
-      routerLink: file.fileName.replace('.md', '')
+      routerLink: file.fileName.replace('.md', ''),
     };
-
   }
 }
